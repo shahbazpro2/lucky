8008ade93330de994d61eee28234939e87e90eee
shahbaz <shahbazrock0321@gmail.com>
2021-12-19 01:43:32 +0500

    Change Home to Home_pg

---
+++ src/pages/Home_pg.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '../components/pages/home/Home'
+
+const Home_pg = () => {
+    return (
+        <div>
+            <Home />
+        </div>
+    )
+}
+
+export default Home_pg
