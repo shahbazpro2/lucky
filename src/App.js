4a32beecc0770727308635f55d5515ed18a6c273
shahbaz <shahbazrock0321@gmail.com>
2021-12-19 01:42:49 +0500

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+import Header from './components/common/Header';
+import Home_pg from './pages/Home_pg';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home_pg />} />
       </Routes>
     </div>
   );
