b0bd548740ebbe51e0167dc65a093bc18b6e817f
shahbaz <shahbazrock0321@gmail.com>
2021-12-19 01:43:05 +0500

    Make Header component

---
+++ src/components/common/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='h-[72px] shadow-md'>
+            <div className="container mx-auto h-full flex items-center">
+                <div className='text-lg'>Logo</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
