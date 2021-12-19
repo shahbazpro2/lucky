e3486bcd52fda902b3ef54236b41cd5380c52da3
shahbaz <shahbazrock0321@gmail.com>
2021-12-20 01:14:13 +0500

    Add more routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/common/Header';
+import Contact_pg from './pages/Contact_pg';
 import Home_pg from './pages/Home_pg';
+import Pool_pg from './pages/Pool_pg';
+import Wallet_pg from './pages/Wallet_pg';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home_pg />} />
+        <Route path="/wallet" element={<Wallet_pg />} />
+        <Route path="/pool" element={<Pool_pg />} />
+        <Route path="/contact" element={<Contact_pg />} />
       </Routes>
     </div>
   );
