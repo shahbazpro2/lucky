8dd98748e9ca7767a2227b1e44b32c6561788c4b
shahbaz <shahbazrock0321@gmail.com>
2021-12-20 01:14:03 +0500

    Add links

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className='h-[72px] shadow-md'>
-            <div className="container mx-auto h-full flex items-center">
-                <div className='text-lg'>Logo</div>
+        <div className='h-[72px] shadow-md shadow-gray-400 bg-gray-800 text-white'>
+            <div className="container px-20 mx-auto h-full flex items-center">
+                <Link to="/"><div className='text-xl cursor-pointer'>LuckyApp</div></Link>
+                <div className="ml-auto space-x-7 flex">
+                    <Link to="/pool">  <div className="cursor-pointer">Pool Information</div></Link>
+                    <Link to="/wallet"> <div className="cursor-pointer">Wallet Contents</div></Link>
+                    <Link to="/contact"> <div className="cursor-pointer">Contact Us</div></Link>
+                </div>
             </div>
 
         </div>
