41004a1a5bf14a51ad4276bd078ddac2f3d35456
shahbaz <shahbazrock0321@gmail.com>
2021-12-22 01:15:40 +0500

    Add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightYellow: '#f6ffab'
+      }
+    },
   },
   plugins: [],
 }
