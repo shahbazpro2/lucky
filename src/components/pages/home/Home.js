55755644878244c74571ba2858f8e8bc56731f6d
shahbaz <shahbazrock0321@gmail.com>
2021-12-22 01:16:02 +0500

    Add sample content

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            home
+        <div className="mt-10 space-y-16">
+            <div>
+                <div className="text-xl mb-5 font-bold">Octupus</div>
+                <div className="bg-lightYellow h-32 shadow-lg p-5 rounded-lg">
+                    hello
+                </div>
+            </div>
+            <div>
+                <div className="text-xl mb-5 font-bold">Octupus</div>
+                <div className="bg-lightYellow h-32 shadow-lg p-5 rounded-lg">
+                    hello
+                </div>
+            </div>
         </div>
     )
 }
