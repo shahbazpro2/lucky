2df77fb372690c57b42442bfc525d0843d6ffb62
shahbaz <shahbazrock0321@gmail.com>
2021-12-22 01:50:00 +0500

    Sample firebase get data test

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getDatabase, ref, get, child } from "firebase/database";
 
 const Home = () => {
+    useEffect(() => {
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, `pool-info`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                console.log(snapshot.val());
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }, [])
     return (
         <div className="mt-10 space-y-16">
             <div>
