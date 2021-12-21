634b972a15d9a661dad946cc385fbe6d16a6319e
shahbaz <shahbazrock0321@gmail.com>
2021-12-22 01:49:30 +0500

    Firebase configurations

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAlHSiLeAf7jjYi8JaIx1Zt4rqTXibWAjw",
+    authDomain: "lucky777-app.firebaseapp.com",
+    databaseURL: "https://lucky777-app-default-rtdb.firebaseio.com",
+    projectId: "lucky777-app",
+    storageBucket: "lucky777-app.appspot.com",
+    messagingSenderId: "6664238257",
+    appId: "1:6664238257:web:1ea494475587ce388c5e90",
+    measurementId: "G-FHLFVQD8H6"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
