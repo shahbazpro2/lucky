afd868ea06f94d6d83cacad0f6e1cd1a38590e98
shahbaz <shahbazrock0321@gmail.com>
2021-12-24 00:43:05 +0500

    Make Detail_pg page

---
+++ src/pages/Detail_pg.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Detail from '../components/pages/detail/Detail'
+
+const Detail_pg = () => {
+    return (
+        <div>
+            <Detail />
+        </div>
+    )
+}
+
+export default Detail_pg
