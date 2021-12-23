26498041b0004ffecf295c788f8cfc9dcfa9bf47
shahbaz <shahbazrock0321@gmail.com>
2021-12-23 12:48:11 +0500

    Map data from firebase

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getDatabase, ref, get, child } from "firebase/database";
 
 const Home = () => {
+    const [data, setData] = useState([])
+
     useEffect(() => {
         const dbRef = ref(getDatabase());
         get(child(dbRef, `pool-info`)).then((snapshot) => {
             if (snapshot.exists()) {
-                console.log(snapshot.val());
+                console.log(snapshot.val()['nft-sections'].list);
+                setData(snapshot.val()['nft-sections'].list)
             } else {
                 console.log("No data available");
             }
@@ -15,19 +18,16 @@ const Home = () => {
         });
     }, [])
     return (
-        <div className="mt-10 space-y-16">
-            <div>
-                <div className="text-xl mb-5 font-bold">Octupus</div>
-                <div className="bg-lightYellow h-32 shadow-lg p-5 rounded-lg">
-                    hello
+        <div className="my-20 grid grid-cols-3 gap-12">
+            {data.map((d, index) => (
+                <div key={index}>
+                    <div className="text-xl mb-5 font-bold">{d.name}</div>
+                    <div className="bg-lightYellow border-[1px] boder-[#e9e9e9] h-[350px] hover:shadow-lg p-5 cursor-pointer rounded-lg flex items-center justify-center">
+                        <img src={d.previewURL} alt="thumbnail" width='300' />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div className="text-xl mb-5 font-bold">Octupus</div>
-                <div className="bg-lightYellow h-32 shadow-lg p-5 rounded-lg">
-                    hello
-                </div>
-            </div>
+            ))}
+
         </div>
     )
 }
