51a3c4ea7e13a0fe6d5f098952e5555bc0293296
shahbaz <shahbazrock0321@gmail.com>
2021-12-24 00:43:36 +0500

    Add detail route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/common/Header';
 import Contact_pg from './pages/Contact_pg';
+import Detail_pg from './pages/Detail_pg';
 import Home_pg from './pages/Home_pg';
 import Pool_pg from './pages/Pool_pg';
 import Wallet_pg from './pages/Wallet_pg';
@@ -14,6 +15,7 @@ function App() {
       <div className='container mx-auto px-20'>
         <Routes>
           <Route path="/" element={<Home_pg />} />
+          <Route path="/detail" element={<Detail_pg />} />
           <Route path="/wallet" element={<Wallet_pg />} />
           <Route path="/pool" element={<Pool_pg />} />
           <Route path="/contact" element={<Contact_pg />} />
