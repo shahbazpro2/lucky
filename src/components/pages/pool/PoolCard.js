cccede8666769acd358d02720a01f4f3016c9d82
shahbaz <shahbazrock0321@gmail.com>
2021-12-28 00:09:50 +0500

    Make content dynamic

--- src/components/pages/pool/PoolCard.js
+++ src/components/pages/pool/PoolCard.js
@@ -1,13 +1,13 @@
 import { Card, CardContent } from '@mui/material'
 import React from 'react'
 
-const PoolCard = () => {
+const PoolCard = ({ title, value }) => {
     return (
         <div>
-            <Card>
+            <Card variant="outlined">
                 <CardContent className="text-center">
-                    <div className="text-xl">0</div>
-                    <div className="text-base">Minted Blocks</div>
+                    <div className="text-xl">{value}</div>
+                    <div className="text-base">{title}</div>
                 </CardContent>
             </Card>
         </div>
