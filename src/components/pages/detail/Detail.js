b297c8f0f8edea5a706877cb5d891a6ed80469ae
shahbaz <shahbazrock0321@gmail.com>
2021-12-30 01:27:13 +0500

    Add modal content

--- src/components/pages/detail/Detail.js
+++ src/components/pages/detail/Detail.js
@@ -9,11 +9,11 @@ const Detail = () => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
     const [open, setOpen] = useState(false)
+    const [singleData, setSingleData] = useState()
 
     useEffect(() => {
         console.log(params);
         (async () => {
-
             const res = await fetchApi('pool-info')
             setLoading(false)
             console.log(res)
@@ -26,6 +26,18 @@ const Detail = () => {
             }
         })()
     }, [])
+
+    const openModal = async (d) => {
+        setSingleData(d)
+        setOpen(true)
+    }
+
+    const getDate = () => {
+        if (singleData?.mintedDateMillis) {
+            const date = new Date(singleData.mintedDateMillis);
+            return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+        }
+    }
 
     return (
         <div className="py-20">
@@ -41,10 +53,8 @@ const Detail = () => {
                                     <Badge color="info" badgeContent={`Sold`} max={999} /> :
                                     <Badge color="info" badgeContent={`${d.priceInLovelace / 1000000}A`} max={999} />
                                 }
-
-
                             </div>
-                            <img className='mx-auto h-20 w-20 cursor-pointer' onClick={() => setOpen(true)} src={d.url} alt="img" />
+                            <img className='mx-auto h-20 w-20 cursor-pointer' onClick={() => openModal(d)} src={d.url} alt="img" />
                             <div className="text-sm">{d.name}</div>
                         </div>
                     ))}
@@ -55,8 +65,26 @@ const Detail = () => {
                 open={open}
                 onClose={() => setOpen(false)}
             >
-                <div className="w-[500px] h-[200px] absolute rltb-0 m-rltb-auto bg-white rounded-lg p-10" >
-                    hello
+                <div className="w-[95%] md:w-[500px] h-[90%] absolute rltb-0 m-rltb-auto bg-white rounded-lg p-10" >
+                    <div className="text-right">{getDate()}</div>
+                    <div className="text-center">
+                        <img src={singleData?.url} className='w-[40%] m-auto' alt="img" />
+                        <div className="text-lg">{singleData?.name}</div>
+                        <div className="mt-10">
+                            <ul className="list-none">
+                                {
+                                    singleData?.variations.map((v, index) => (
+                                        <li key={index} className='grid grid-cols-2 text-left'>
+                                            <div className='font-medium'>{v.split(':')[0]}</div>
+                                            <div className='text-right'>{v.split(':')[1]}</div>
+                                        </li>
+                                    ))
+                                }
+
+                            </ul>
+
+                        </div>
+                    </div>
                 </div>
             </Modal>
         </div>
