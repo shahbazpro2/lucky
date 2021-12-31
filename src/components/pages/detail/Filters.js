8130f6676bddc3ffedc0758b3999fb217944a7d9
shahbaz <shahbazrock0321@gmail.com>
2022-01-01 03:01:42 +0500

    Fix filter

--- src/components/pages/detail/Filters.js
+++ src/components/pages/detail/Filters.js
@@ -1,25 +1,62 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Button, Checkbox, Collapse, List, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
 import FilterListIcon from '@mui/icons-material/FilterList';
+import ListItemButton from '@mui/material/ListItemButton';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const Filters = ({ filtersList }) => {
-
-    filtersList && Object.keys(filtersList)?.forEach(f => console.log(f))
+    const [open, setOpen] = useState('')
+    const handleClick = (val) => {
+        val === open ? setOpen('') : setOpen(val)
+    }
     return (
         <div>
-            <PopupState variant="popover" popupId="demo-popup-menu" placement="center">
+            <PopupState variant="popover">
                 {(popupState) => (
                     <React.Fragment>
                         <Button variant="contained" {...bindTrigger(popupState)}>
                             <FilterListIcon />
                         </Button>
-                        <Menu {...bindMenu(popupState)} transformOrigin="left">
+                        <Menu {...bindMenu(popupState)} anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}>
                             {filtersList && Object.keys(filtersList)?.map((f, index) =>
-                                <MenuItem key={index}>{f}</MenuItem>
+                                <div key={index}>
+                                    {index < 3 ?
+                                        <MenuItem>{f}</MenuItem> :
+                                        <div>
+                                            <ListItemButton onClick={() => handleClick(f)}>
+                                                <ListItemText primary={f} />
+                                                {open === f ? <ExpandLess /> : <ExpandMore />}
+
+                                            </ListItemButton>
+                                            <Collapse in={open === f} timeout="auto" unmountOnExit>
+                                                <List component="div" disablePadding>
+                                                    {filtersList[f].map((l, index) => (
+                                                        <ListItemButton key={index} sx={{ pl: 4 }}>
+                                                            <ListItemText primary={l} />
+                                                            <ListItemIcon>
+                                                                <Checkbox
+                                                                    disableRipple
+                                                                />
+                                                            </ListItemIcon>
+                                                        </ListItemButton>
+                                                    ))}
+                                                </List>
+                                            </Collapse>
+                                        </div>
+                                    }
+                                </div>
+
                             )
                             }
-                            <MenuItem onClick={popupState.close}>Logout</MenuItem>
                         </Menu>
                     </React.Fragment>
                 )}
